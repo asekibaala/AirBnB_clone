2e87f68d5d0b73a34f7053f9295af1d4bdc1d705
mukisa <mukisa@Ubuntu20.4.myguest.virtualbox.org>
2023-08-10 06:29:02 +0300

    phase 1 of baseModel complete

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 that will be used but several instances of the class"""
 import uuid
 from datetime import datetime
-import json
 
 class BaseModel():
     """Defines common attributes that will be used in other
@@ -11,19 +10,9 @@ class BaseModel():
     """
     def __init__(self):
         """initializes class"""
-        # if kwargs:
-        #     for key, value in kwargs.items():
-        #         if key == 'created_at' or key == 'updated_at':
-        #             value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-        #         if key != '__class__':
-        #             setattr(self, key, value)
-        # else:
-        #     self.id = str(uuid.uuid4())
-        #     self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        #     self.updated_at = self.created_at
         self.id = str(uuid.uuid4)
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
             
         
     
@@ -34,14 +23,12 @@ class BaseModel():
         )
             
     def save(self):
-        self.updated_at = datetime.now()
-        # data = json.dumps(self.__dict__)
-        # with open('data.json', 'w') as file:
-        #     file.write(data)
+        self.updated_at = datetime.now().isoformat()
+        
     
     def to_dict(self):
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = type(self).__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['created_at'] = self.created_at
+        dictionary['updated_at'] = self.updated_at
         return dictionary
