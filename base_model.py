7c867a718fcb16becf17faea23f1407f30d34411
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-08-10 08:09:10 -0700

    Creating new base class

---
+++ base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines BaseModel claass which hold common attributes
+that will be used but several instances of the class"""
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """Defines common attributes that will be used in other
+    instances.
+    """
+    def __init__(self, *args, **kwargs):
+        """initializes class"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().isoformat()
+        
+    
+    def __str__(self):
+        """string representation of base model"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__
+        )
+            
+    def save(self):
+        self.updated_at = datetime.now().isoformat()
+        
+    
+    def to_dict(self):
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = type(self).__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
